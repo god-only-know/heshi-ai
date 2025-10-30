584cecb8e130200c0e4ea2f837af1578de376233
linjianan <1361484227@qq.com>
2025-10-30 17:15:24 +0800

    Updates

--- assets/index-CoiNKH1W.js
+++
@@ -1,1 +0,0 @@
-import{d as e,R as t,S as n}from"./index-VDyzxJPc.js";const s=e({__name:"index",setup(o){return console.log("activity"),(a,c)=>(n(),t("div",null," chat "))}});export{s as default};
--- assets/use-id-B7pO8jeo.js
+++
@@ -1,1 +0,0 @@
-import{N as e}from"./index-VDyzxJPc.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return"".concat(t,"-").concat(++r)}export{u};
